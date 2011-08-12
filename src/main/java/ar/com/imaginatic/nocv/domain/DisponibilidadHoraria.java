153dbb52d5ca060eea0b655dbb8c9250a145d0b5
cristianlp <cristianlp@gmail.com>
2011-08-12 10:48:06 -0300

    Se agrega DisponibilidadHoraria class

---
+++ src/main/java/ar/com/imaginatic/nocv/domain/DisponibilidadHoraria.java
@@ -0,0 +1,24 @@
+package ar.com.imaginatic.nocv.domain;
+
+public class DisponibilidadHoraria {
+
+	private String oid;
+	private String descripcion;
+
+	public String getOid() {
+		return oid;
+	}
+
+	public void setOid(String oid) {
+		this.oid = oid;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+}
